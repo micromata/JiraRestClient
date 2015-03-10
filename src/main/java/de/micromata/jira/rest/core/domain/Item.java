53b3d51bda47da0f81f5ae4ddd091ae2278ec61f
vfilippow <v.filippow@micromata.de>
2015-03-10 14:52:26 +0100

    added missing getter methods

--- src/main/java/de/micromata/jira/rest/core/domain/Item.java
+++ src/main/java/de/micromata/jira/rest/core/domain/Item.java
@@ -1,44 +1,79 @@
-
 package de.micromata.jira.rest.core.domain;
 
 import com.google.gson.annotations.Expose;
 
 public class Item {
 
-    @Expose
-    private String field;
-    @Expose
-    private String fieldtype;
-    @Expose
-    private String from;
-    @Expose
-    private String fromString;
-    @Expose
-    private String to;
-    @Expose
-    private String toString;
+	@Expose
+	private String	field;
+	@Expose
+	private String	fieldtype;
+	@Expose
+	private String	from;
+	@Expose
+	private String	fromString;
+	@Expose
+	private String	to;
+	@Expose
+	private String	toString;
 
-    public void setField(String field) {
-        this.field = field;
-    }
+	public void setField(String field) {
 
-    public void setFieldtype(String fieldtype) {
-        this.fieldtype = fieldtype;
-    }
+		this.field = field;
+	}
 
-    public void setFrom(String from) {
-        this.from = from;
-    }
+	public void setFieldtype(String fieldtype) {
 
-    public void setFromString(String fromString) {
-        this.fromString = fromString;
-    }
+		this.fieldtype = fieldtype;
+	}
 
-    public void setTo(String to) {
-        this.to = to;
-    }
+	public void setFrom(String from) {
 
-    public void setToString(String toString) {
-        this.toString = toString;
-    }
+		this.from = from;
+	}
+
+	public void setFromString(String fromString) {
+
+		this.fromString = fromString;
+	}
+
+	public void setTo(String to) {
+
+		this.to = to;
+	}
+
+	public void setToString(String toString) {
+
+		this.toString = toString;
+	}
+
+	public String getField() {
+
+		return field;
+	}
+
+	public String getFieldtype() {
+
+		return fieldtype;
+	}
+
+	public String getFrom() {
+
+		return from;
+	}
+
+	public String getFromString() {
+
+		return fromString;
+	}
+
+	public String getTo() {
+
+		return to;
+	}
+
+	public String getToString() {
+
+		return toString;
+	}
 }
