5a18978aa46fe6d79b0e74d311f7d66c797b5aed
vfilippow <v.filippow@micromata.de>
2015-03-12 15:17:32 +0100

    Memory leak workaround fix for JIRA Monitor (Opportunity to close generated Threadpool)

--- src/main/java/de/micromata/jira/rest/client/SearchClient.java
+++ src/main/java/de/micromata/jira/rest/client/SearchClient.java
@@ -1,24 +1,25 @@
 package de.micromata.jira.rest.client;
 
+import java.io.IOException;
+import java.util.concurrent.Future;
+
 import de.micromata.jira.rest.core.domain.JqlSearchResult;
 import de.micromata.jira.rest.core.jql.JqlSearchBean;
 import de.micromata.jira.rest.core.util.RestException;
 
-import java.io.IOException;
-import java.util.concurrent.Future;
-
 /**
- * User: Christian Schulze
- * Email: c.schulze@micromata.de
- * Date: 31.07.2014
+ * User: Christian Schulze Email: c.schulze@micromata.de Date: 31.07.2014
  */
 public interface SearchClient {
 
-    /**
-     * Performs an extended search for issues given by the project.
-     *
-     * @return list of issues
-     * @throws de.micromata.jira.rest.core.util.RestException
-     */
-    public Future<JqlSearchResult> searchIssues(JqlSearchBean jsb) throws RestException, IOException;
+	/**
+	 * Performs an extended search for issues given by the project.
+	 *
+	 * @return list of issues
+	 * @throws de.micromata.jira.rest.core.util.RestException
+	 */
+	public Future<JqlSearchResult> searchIssues(JqlSearchBean jsb)
+			throws RestException, IOException;
+
+	public void close();
 }
